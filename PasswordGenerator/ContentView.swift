7a68cceffbcd5becbe725f793c185e1f56f44291
NR <nicholas.rebello@gmail.com>
2024-04-23 21:36:20 -0400

    Initial Commit

---
+++ PasswordGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PasswordGenerator
+//
+//  Created by Nicholas Rebello on 2024-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Nicholas Rebello on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
